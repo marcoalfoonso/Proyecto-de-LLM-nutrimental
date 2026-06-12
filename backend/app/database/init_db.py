7d91381dded095271eb7983f588526eb3248387d
RenBadillo <reny18badillo@gmail.com>
2026-06-11 20:01:09 -0600

    user_repository.py se encarga de Guardar usuarios, buscar usuarios y actualizar usuarios.

---
+++ backend/app/database/init_db.py
@@ -0,0 +1,52 @@
+from database.database import (
+    get_connection
+)
+
+
+def initialize_database():
+
+    conn = get_connection()
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+        whatsapp_number TEXT UNIQUE,
+
+        name TEXT,
+
+        age INTEGER,
+
+        sex TEXT,
+
+        weight REAL,
+
+        height REAL,
+
+        activity_level TEXT,
+
+        goal TEXT,
+
+        dietary_restrictions TEXT,
+
+        food_preferences TEXT,
+
+        budget REAL
+    )
+    """)
+
+    conn.commit()
+
+    conn.close()
+
+
+if __name__ == "__main__":
+
+    initialize_database()
+
+    print(
+        "Base de datos inicializada"
+    )
---
+++ backend/app/database/user_repository.py
@@ -0,0 +1,85 @@
+import json
+
+from database.database import (
+    get_connection
+)
+
+from models.user_profile import (
+    UserProfile
+)
+
+
+class UserRepository:
+
+    def create(
+        self,
+        user: UserProfile,
+        whatsapp_number: str
+    ):
+
+        conn = get_connection()
+
+        cursor = conn.cursor()
+
+        cursor.execute(
+            """
+            INSERT INTO users (
+
+                whatsapp_number,
+
+                name,
+
+                age,
+
+                sex,
+
+                weight,
+
+                height,
+
+                activity_level,
+
+                goal,
+
+                dietary_restrictions,
+
+                food_preferences,
+
+                budget
+
+            )
+
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """,
+            (
+                whatsapp_number,
+
+                user.name,
+
+                user.age,
+
+                user.sex,
+
+                user.weight,
+
+                user.height,
+
+                user.activity_level,
+
+                user.goal,
+
+                json.dumps(
+                    user.dietary_restrictions
+                ),
+
+                json.dumps(
+                    user.food_preferences
+                ),
+
+                user.budget
+            )
+        )
+
+        conn.commit()
+
+        conn.close()
